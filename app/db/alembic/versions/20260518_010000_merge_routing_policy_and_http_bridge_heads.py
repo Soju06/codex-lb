93dad88437ece789f1f867b1bdffaba25499253d
Darafei Praliaskouski <me@komzpa.net>
2026-05-18 19:18:32 +0400

    style(db): wrap migration merge docstring

--- app/db/alembic/versions/20260518_010000_merge_routing_policy_and_http_bridge_heads.py
+++ app/db/alembic/versions/20260518_010000_merge_routing_policy_and_http_bridge_heads.py
@@ -1,7 +1,8 @@
 """merge routing policy and HTTP bridge heads
 
 Revision ID: 20260518_010000_merge_routing_policy_and_http_bridge_heads
-Revises: 20260515_010000_merge_routing_policy_and_request_log_heads, 20260518_000000_add_http_bridge_durable_input_prefix
+Revises: 20260515_010000_merge_routing_policy_and_request_log_heads,
+    20260518_000000_add_http_bridge_durable_input_prefix
 Create Date: 2026-05-18
 """
 
